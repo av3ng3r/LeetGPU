2661400b2180d5bd1126068663f949d9aad8ac01
av3ng3r <iabinashmohapatra@gmail.com>
2025-12-31 16:58:54 +0530

    3.Matrix Transpose

---
+++ 3.Matrix Transpose/matrix_transpose.cu
@@ -0,0 +1,25 @@
+
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+__global__ void matrix_transpose_kernel(const float* input, float* output, int rows, int cols) {
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+    // printf("%d : %d\n", row, col);
+
+    if (row < rows && col < cols) {
+        output[col * rows + row] = input[row * cols + col];
+        // printf("row=%d col=%d index=%.1f\n", row, col, input[row * cols + col]);
+    }
+}
+
+// input, output are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const float* input, float* output, int rows, int cols) {
+    dim3 threadsPerBlock(16, 16);
+    dim3 blocksPerGrid((cols + threadsPerBlock.x - 1) / threadsPerBlock.x,
+                       (rows + threadsPerBlock.y - 1) / threadsPerBlock.y);
+
+    matrix_transpose_kernel<<<blocksPerGrid, threadsPerBlock>>>(input, output, rows, cols);
+    cudaDeviceSynchronize();
+}
