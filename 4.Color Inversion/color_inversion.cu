c7b12052967a9ccc153d6feb891d60d2f6b99c2d
av3ng3r <iabinashmohapatra@gmail.com>
2026-01-01 19:47:11 +0530

    4.Color Inversion

---
+++ 4.Color Inversion/color_inversion.cu
@@ -0,0 +1,19 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+__global__ void invert_kernel(unsigned char* image, int width, int height) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(idx < width * height * 4 && idx%4 != 3) {
+        // printf("%d => %d\n", idx, (int)image[idx]);
+        image[idx] = (unsigned char)(255 - (int)image[idx]);
+    }
+}
+// image_input, image_output are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(unsigned char* image, int width, int height) {
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (width * height + threadsPerBlock - 1) / threadsPerBlock;
+
+    invert_kernel<<<blocksPerGrid, threadsPerBlock>>>(image, width, height);
+    cudaDeviceSynchronize();
+}
